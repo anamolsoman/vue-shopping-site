ea61c7a219d61ba2ff666d751d1257c39532c34d
anamolsoman <anamol@astutertech.com>
2021-09-24 15:32:26 +0530

    About section design added.

---
+++ components/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="about-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-sm-12 col-xs-12 d-flex">
+          <div class="about-image">
+            <img :src="require('static/img/watch-02.jpg')" alt="about-image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+.about-section {
+  padding: 100px;
+}
+.about-section .about-image img {
+  max-width: 450px;
+  width: 100%;
+}
+.about-section .about-image {
+  margin: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
@@ -12,6 +14,7 @@
     </head>
     <Header />
     <Home />
+    <About />
   </div>
 </template>
 
