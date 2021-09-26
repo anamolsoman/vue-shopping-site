90a55833ad2f7aac92a9279b4e24adb4f5e36479
anamolsoman <anamol@astutertech.com>
2021-09-26 20:19:45 +0530

    Bug fixed.

--- components/About.vue
+++ components/About.vue
@@ -2,7 +2,7 @@
   <section class="about-section">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 col-sm-12 col-xs-12 d-flex">
+        <div class="col-lg-6 col-sm-12 col-xs-12">
           <div class="about-image">
             <img :src="require('static/img/watch-02.jpg')" alt="about-image" />
           </div>
@@ -11,7 +11,7 @@
     </div>
   </section>
 </template>
-<style scoped>
+<style>
 .about-section {
   padding: 100px;
 }
