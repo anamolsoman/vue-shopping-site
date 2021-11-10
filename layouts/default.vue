c848d1bdab66eb634d7a2cf2b969b554e307ab05
anamolsoman <anamol@astutertech.com>
2021-11-10 08:35:28 +0530

    Major theme changes.

---
+++ layouts/default.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed app elevation="1" dark class="primary">
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
+      />
+      <v-spacer class="hidden-sm-and-down" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn class="ma-1 hidden-sm-and-down" to="/" text>Home</v-btn>
+      <v-btn class="ma-1 hidden-sm-and-down" to="/products" text
+        >Products</v-btn
+      >
+
+      <v-btn class="ma-1 hidden-sm-and-down" to="/testimonials" text
+        >Testimonials</v-btn
+      >
+
+      <v-btn class="ma-1 hidden-sm-and-down" to="/about" text>About</v-btn>
+
+      <v-btn class="ma-1 hidden-sm-and-down" to="/contact" text>Contact</v-btn>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+
+    <!-- <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Mobile Store',
+    }
+  },
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap');
+#app {
+  margin: 0;
+  font-size: 16px;
+  font-family: 'Montserrat', sans-serif !important;
+  font-weight: 400;
+}
+* {
+  margin: 0;
+  padding: 0;
+  outline: none;
+}
+a {
+  text-decoration: none !important;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,80 @@
 <template>
-  <div>
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-      />
-      <!-- <link rel="stylesheet" href="../style/responsive.css" /> -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </head>
-    <Header />
-    <Home />
-    <About />
-  </div>
+  <section class="home-section primary">
+    <v-container>
+      <v-row justify="center">
+        <v-col md="4" lg="4" cols="12" order-md="2" order-lg="2">
+          <div class="home-img">
+            <img :src="require('@/static/img/watch-01.png')" />
+          </div>
+        </v-col>
+        <v-col md="4" lg="4" cols="12" order-md="1" order-lg="1">
+          <div class="home-content">
+            <h1>Present your awesome product</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <a href="#" class="btn mybtn1">Buy For $150</a>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
-
-<script>
-export default {}
-</script>
 <style>
-@import '../style/responsive.css';
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap');
-body {
-  margin: 0;
-  font-size: 16px;
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 400;
-}
-* {
-  margin: 0;
-  padding: 0;
-  outline: none;
-}
-a {
-  text-decoration: none !important;
-}
-.header,
 .home-section {
-  background-color: #d70d25;
+  padding: 150px 0px 150px;
+}
+.home-section h1 {
+  color: white;
+  font-weight: 700;
+  font-size: 55px;
+  margin: 0;
+  line-height: 60px;
+}
+.home-section p {
+  color: white;
+  font-size: 18px;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  line-height: 28px;
+}
+.home-section .btn {
+  background-color: white;
+  border-radius: 30px;
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 12px 30px;
+}
+.home-section .btn:hover {
+  background-color: black;
+  color: white;
+}
+/* .home-section .home-img {
+  position: relative;
+} */
+.home-section .home-img img {
+  width: 100%;
+  max-width: 400px;
+  left: 150px;
+  /* position: absolute; */
+  top: -50px;
+  margin: auto;
+  animation: productani01 5s linear infinite;
+}
+.home-img {
+  display: flex;
+}
+@keyframes productani01 {
+  0%,
+  100% {
+    transform: translateY(15px);
+  }
+  50% {
+    transform: translateY(-15px);
+  }
 }
 </style>
